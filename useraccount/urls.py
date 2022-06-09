5d00de79466271b5dfe7e30a263367352841be75
NirajanJoc <Nirajanjoc19@gmail.com>
2022-06-09 15:07:56 +0545

    'login_required'

--- useraccount/urls.py
+++ useraccount/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from useraccount.views import login_view, register_view,dashboard
+from useraccount.views import login_view, register_view, dashboard, logout_view
 
 app_name='user'
 
@@ -7,4 +7,5 @@ urlpatterns=[
     path('login/', login_view, name='login'),
     path('register/',register_view, name='register'),
     path('dashboard/',dashboard, name='dashboard'),
+    path('logout/', logout_view, name='logout'),
 ]
--- useraccount/views.py
+++ useraccount/views.py
@@ -1,35 +1,48 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.forms import AuthenticationForm
 from useraccount.forms import SignUpForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+@login_required(login_url='user:login')
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('user:login'))
     
 def login_view(request):
-    form= AuthenticationForm(request.POST or None)
-    if request.POST:
-        username=request.POST.get("username")
-        password=request.POST.get('password')
-        user=authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            #return HttpResponseRedirect(reverse(''))
-            return HttpResponseRedirect('/admin/')
-        else:
-            messages.info(request, 'Username or Password is Incorrect')
-    context={'form':form}
-    return render(request, 'login.html', context)
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('user:dashboard'))
+    else:
+        form= AuthenticationForm(request.POST or None)
+        if request.POST:
+            username=request.POST.get("username")
+            password=request.POST.get('password')
+            user=authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                #return HttpResponseRedirect(reverse(''))
+                return HttpResponseRedirect(reverse('user:dashboard'))
+            else:
+                messages.info(request, 'Username or Password is Incorrect')
+        context={'form':form}
+        return render(request, 'login.html', context)
 
 def register_view(request):
-    form=SignUpForm(request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            user=form.cleaned_data.get('username')
-            messages.success(request, user+ ', Your account have successfully created.')
-            return HttpResponseRedirect(reverse('user:login'))
-    context={'form':form}
-    return render(request, 'register.html' ,context)
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('user:dashboard'))
+    else:
+        form=SignUpForm(request.POST or None)
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                user=form.cleaned_data.get('username')
+                messages.success(request, user+ ', Your account have successfully created.')
+                return HttpResponseRedirect(reverse('user:login'))
+        context={'form':form}
+        return render(request, 'register.html' ,context)
